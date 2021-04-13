5e696f8a2c1aa7cb05c2902638529e3fe8ecf0b6
leah_carlin <leahrcarlin@gmail.com>
2021-04-13 16:18:21 +0200

    Now showing random image

--- js/script.js
+++ js/script.js
@@ -3,11 +3,38 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
+/*connect API & get random images*/
 const getImage = async function (){
     const res = await fetch(
         "https://picsum.photos/v2/list?limit=100"
     );
     const images = await res.json();
     console.log(images);
+    selectRandomImage(images);
 }
-getImage();
+
+
+/* get random image */
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    //console.log(randomIndex);
+    const randomImage = images[randomIndex];
+    //console.log(randomImage);
+    displayImage(randomImage);
+}
+
+/* display image */
+const displayImage = function (randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+};
+
+/* click button */
+button.addEventListener("click", function(){
+    getImage();
+});
+
